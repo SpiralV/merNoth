e8fbed7d7dfe32be52c710622926c8bdf483ffb6
SpiralV <MVelasco90@hotmail.com>
2021-07-08 10:02:40 -0700

    serverfolder

---
+++ controllers/api-v1/authLockedRoute.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const db = require('../../models')
+
+const authLockedRoute = async () => {
+    try {
+        // get the jwt from auth headers
+        const authHeaders = req.headers.authorization
+        // verify the jwt -- if the jwt is not valid will throw to catch
+        const decoded = jwt.verify(authHeader. process.env.JWT_SECRET)
+        // find the user from the db
+        const foundUser = await db.User.findById(decoded.id)
+        // mount the user on the res.locals
+        res.locals.user = foundUser
+        next()
+    } catch (err) {
+        console.log(err)
+        res.status(401).json({ msg: 'begone from this place' })
+    }
+}
+
+module.exports = authLockedRoute
---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const rowdy = require('rowdy-logger')
+
+//connect to db
+const db = require('./models')
+db.connect()
+
+// config express app
+const app = express()
+const PORT = process.env.PORT || 3001
+const rowdyResults = rowdy.begin(app)
+
+// middling wares
+app.use(cors())
+// body parser middlewares
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json()) // for the request body
+
+//custom middleware
+app.use((req, res, next) => {
+    console.log('hello from a middleware! 🥨')
+    next()
+})
+
+//controllers
+app.use('/api-v1/users', require('./controllers/api-v1/users.js'))
+
+app.get('/', (req, res) => {
+    res.json({ msg: 'hello from the backend! '})
+})
+// listen on a port
+app.listen(PORT, () => {
+    rowdyResults.print()
+    console.log(`liznin on port ${PORT} 🐱‍🚀`)
+})
